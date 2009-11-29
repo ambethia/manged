1cf8f0aaf0399cca51be8d2d7a688bd31bbc7a58
Jason L Perry <jasper@ambethia.com>
2009-11-28 23:42:11 -0500

    More on Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,16 @@ class ItemsController < ApplicationController
 
   private
 
+  def build_object
+    if params[:copy] && copy = Item.find_by_entry(params[:copy])
+      @current_object = copy.clone
+    else
+      @current_object = Item.new
+    end
+  end
+
   def current_objects
     @current_objects = current_model.paginate :page => params[:page], :per_page => 100,
-      :order => "name ASC"
+      :order => "entry DESC"
   end
 end
